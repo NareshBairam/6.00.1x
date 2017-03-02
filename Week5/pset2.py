2850c556c6ef387a036b0f86ebdffd580b904b86
NareshBairam <bairamnaresh7@gmail.com>
2017-03-02 16:13:58 +0530

    Week5 Problem 2

---
+++ Week5/pset2.py
@@ -0,0 +1,24 @@
+def updateHand(hand, word):
+    """
+    Assumes that 'hand' has all the letters in word.
+    In other words, this assumes that however many times
+    a letter appears in 'word', 'hand' has at least as
+    many of that letter in it. 
+
+    Updates the hand: uses up the letters in the given word
+    and returns the new hand, without those letters in it.
+
+    Has no side effects: does not modify hand.
+
+    word: string
+    hand: dictionary (string -> int)    
+    returns: dictionary (string -> int)
+    """
+    # TO DO ... <-- Remove this comment when you code this function
+    newhand = {}
+    newhand = hand.copy()
+    for i in word:
+            newhand[i] = newhand.get(i, 1) - 1
+    return newhand
+    
+    
